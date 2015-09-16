3965be69c5099908f7523d65fc3c9b257a8b13b4
Hengjian Jia <henryjia18@yahoo.com>
2015-09-16 19:59:39 +0100

    Optimises code by reducing repeated memory accesses by increasing block size.

--- cudadefs.h
+++ cudadefs.h
@@ -1,6 +1,6 @@
 #define CC 52
-#define BLOCK_DIM1 64
-#define BLOCK_DIM2 16
+#define BLOCK_DIM1 1024
+#define BLOCK_DIM2 32
 
 #ifndef IDX2C // For Column Major
 #define IDX2C(i,j,ld) (j * ld + i) // i is column, j is row, ld is total number of columns
